c5ae2bcc5d10ef3e108f2fb3bcffe720faa4f763
Dean Abbott <deanabbott293@gmail.com>
2021-11-18 13:48:48 +0900

    basic static page complete w no styke

--- Scrimba/first-react-app/src/index.js
+++ Scrimba/first-react-app/src/index.js
@@ -2,5 +2,15 @@ import react from 'react'
 import ReactDOM from 'react-dom'
 
 const page = (
+  <div>
+    <img src="images/react-logo.png" alt="react logo" width="40px"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by in Jordan Walke</li>
 
+    </ul>
+  </div>
 )
+
+ReactDOM.render(page, document.getElementById('root'))

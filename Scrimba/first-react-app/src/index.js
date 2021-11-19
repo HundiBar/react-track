3dd6ac58306e0646d8879d8adf5e4bea42f6715a
Dean Abbott <deanabbott293@gmail.com>
2021-11-19 12:43:56 +0900

    parent child components

--- Scrimba/first-react-app/src/index.js
+++ Scrimba/first-react-app/src/index.js
@@ -1,7 +1,7 @@
 import react from 'react'
 import ReactDOM from 'react-dom'
 
-function RestOfContent() {
+function MainContent() {
   return (
     <div>
       <h1>Reasons im excited to learn React</h1>
@@ -34,7 +34,7 @@ function Page() {
   return (
     <div>
       <Header/>
-      <RestOfContent/>
+      <MainContent/>
       <Footer/>
 
     </div>

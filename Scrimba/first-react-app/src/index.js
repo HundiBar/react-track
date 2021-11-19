5ed7798ef8bd9ce35caf6f6aaa3a514418f872bf
Dean Abbott <deanabbott293@gmail.com>
2021-11-19 12:33:14 +0900

    parent child components

--- Scrimba/first-react-app/src/index.js
+++ Scrimba/first-react-app/src/index.js
@@ -1,24 +1,29 @@
 import react from 'react'
 import ReactDOM from 'react-dom'
 
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="images/react-logo.png" alt="not working" width="40px" />
+      </nav>
+    </header>
+  )
+}
+
 function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="/react-logo.png" alt="not working" width="40px"/>
-        </nav>
-      </header>
+      <Header/>
       <h1>Reasons im excited to learn React</h1>
       <ol>
         <li>reason 1</li>
         <li>reason 2</li>
         <li>reason 3</li>
       </ol>
-      <footer>"© 2021 Abbott development. All rights reserved."</footer>
+      <footer>© 2021 Abbott development. All rights reserved.</footer>
     </div>
   )
 }
-
 
 ReactDOM.render(< Page />, document.getElementById('root'))

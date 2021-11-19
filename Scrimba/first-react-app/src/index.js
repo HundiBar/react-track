e3748a0e7496b02a5345cef0d0dc98d2ec404b86
Dean Abbott <deanabbott293@gmail.com>
2021-11-19 12:23:10 +0900

    basic components

--- Scrimba/first-react-app/src/index.js
+++ Scrimba/first-react-app/src/index.js
@@ -6,14 +6,16 @@ function Page() {
     <div>
       <header>
         <nav>
-          <img src="/react-logo.png" alt="not working"/>
+          <img src="/react-logo.png" alt="not working" width="40px"/>
         </nav>
       </header>
+      <h1>Reasons im excited to learn React</h1>
       <ol>
         <li>reason 1</li>
         <li>reason 2</li>
         <li>reason 3</li>
       </ol>
+      <footer>"© 2021 Abbott development. All rights reserved."</footer>
     </div>
   )
 }

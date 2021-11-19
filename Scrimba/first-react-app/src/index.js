bbdbbe5c43faea50584dc97e184ee8a0f891387d
Dean Abbott <deanabbott293@gmail.com>
2021-11-19 12:20:50 +0900

    basic components

--- Scrimba/first-react-app/src/index.js
+++ Scrimba/first-react-app/src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom'
 function Page() {
   return (
     <div>
+      <header>
+        <nav>
+          <img src="/react-logo.png" alt="not working"/>
+        </nav>
+      </header>
       <ol>
         <li>reason 1</li>
         <li>reason 2</li>

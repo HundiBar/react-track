e663554da780b428d2e3f4415735c8d22e863421
Dean Abbott <deanabbott293@gmail.com>
2021-11-24 15:53:18 +0900

    airbnb initial push for react

--- Scrimba/airbnb-clone/src/App.js
+++ Scrimba/airbnb-clone/src/App.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import Hero from "./Hero"
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }

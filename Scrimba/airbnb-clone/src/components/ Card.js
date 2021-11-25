e20d24b04c8da0731c55afe4f5bd4d24c262c116
Dean Abbott <deanabbott293@gmail.com>
2021-11-25 15:48:50 +0900

    airbnb card component

--- Scrimba/airbnb-clone/src/components/ Card.js
+++ Scrimba/airbnb-clone/src/components/ Card.js
@@ -4,6 +4,10 @@ export default function Card() {
   return (
     <div className="card">
       <img src="../images/katie-zaferes.png" alt="No Katie" className="card--image"/>
+      <div className="card--stats">
+        <img src="../images/star.png" alt="no star"/>
+
+      </div>
     </div>
   )
 }

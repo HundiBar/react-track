eb146b183735e94a8ce0c2cb0a5b8eda8537e60d
Dean Abbott <deanabbott293@gmail.com>
2021-11-27 15:49:01 +0900

    setting up props data file

--- Scrimba/airbnb-clone/src/App.js
+++ Scrimba/airbnb-clone/src/App.js
@@ -2,14 +2,26 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+  const cards = data.map(item => {
+    return (
+      < Card
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location = {item.location}
+        price = {item.price}
+
+      />
+    )
+  })
   return (
     <div>
       <Navbar />
       <Hero />
       <Card />
-      // use info from data.js to set props.
     </div>
   )
 }

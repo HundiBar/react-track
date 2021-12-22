f171bb611c3c2e97ba923d9ee49600a924e35ca9
Dean Abbott <deanabbott293@gmail.com>
2021-12-22 14:29:08 +0900

    api call

--- Scrimba/meme-generator/src/components/Meme.js
+++ Scrimba/meme-generator/src/components/Meme.js
@@ -34,7 +34,7 @@ export default function Meme() {
     fetch('https://api.imgflip.com/get_memes')
     .then(response =>  response.json())
     .then(data => console.log(data));
-  },[meme])
+  },[])
 
   return (
     <main>

69d8d94a18283faf5da8a5a93c01aabd56365a98
Dean Abbott <deanabbott293@gmail.com>
2021-12-22 14:10:24 +0900

    dynamic text change react forms

--- Scrimba/meme-generator/src/components/Meme.js
+++ Scrimba/meme-generator/src/components/Meme.js
@@ -10,7 +10,7 @@ export default function Meme() {
 
   const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
-  function getMeme() {
+  const getMeme = () => {
     const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     const url = memesArray[randomNumber].url
@@ -18,6 +18,10 @@ export default function Meme() {
       ...prevMeme,
       randomImage: url
     }))
+
+  const handleChange = (event) => {
+      const {name, value} = event.target
+  }
 
   }
 
@@ -28,11 +32,15 @@ export default function Meme() {
           type="text"
           placeholder="Top text"
           className="form--input"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Bottom text"
           className="form--input"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
         <button
           className="form--button"
@@ -42,6 +50,8 @@ export default function Meme() {
         </button>
       </div>
       <img src={meme.randomImage} className="meme--image" alt="not pic"/>
+      <h2 className="meme--text top">{meme.topText}</h2>
+      <h2 className="meme--text bottom">{meme.bottomText}</h2>
     </main>
   )
 }
